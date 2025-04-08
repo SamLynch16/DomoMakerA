ca645cf4a5bb0ae5d4f9ae2667c0d375beffc487
SamLynch16 <spl8631@rit.edu>
2025-04-08 18:51:49 -0400

    first commit

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const espressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+
+const router = require('./router.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://spl8631:Indiais1%2A@igme430.zffgtrc.mongodb.net/DonoMaker?retryWrites=true&w=majority&appName=IGME430';
+mongoose.connect(dbURI).catch((err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+const app = express();
+
+app.use(helmet());
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('handlebars', espressHandlebars.engine({ defaultLayout: '' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) { throw err; }
+  console.log(`Listening on port ${port}`);
+});
